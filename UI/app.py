c221955f27424f2f5077e8fd360d09e3d7fabbb9
DulanjayaChathura <sacdulanjaya.16@cse.mrt.ac.lk>
2020-06-29 22:47:05 +0530

    BUGFIX: fixing is not done

--- UI/app.py
+++ UI/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, redirect, url_for, request,render_template
 from elasticsearch import Elasticsearch
 from sinling import SinhalaTokenizer,preprocess, word_joiner,word_splitter
-import Levenshtein as lev
+
 
 
 
@@ -11,11 +11,18 @@ tokenizer = SinhalaTokenizer()
 es = Elasticsearch()
 hits=[]
 lyrcs_list=[]
-music_by=["සංගීත","අධ්‍යක්ෂණය","තනු"]
-lyrics_by=["රචිත", "රචනය","ලියන"]
+processed_query_request=""
+
+music_by_list=["සංගීත","අධ්‍යක්ෂණය","තනු"]
+lyrics_by_list=["රචිත", "රචනය","ලියන","ලියූ"]
 genere_list =["පැරණි","ඉල්ලීම","චිත්‍රපට","ක්ලැසික්","පොප්ස්","කැලිප්සෝ","ගෝල්ඩන් ඕල්ඩීස්","යුගල","පොප්","උද්වේගකර"]
 key_list =["major","minor"]
+artist_list=["ගේ","ගැයූ","ගායනා කල","ගායනා කරන"]
+# possetion_list=["","","",]
 
+popular_list=["ජනප්‍රිය","හොඳම","ප්‍රසිද්ධ","ප්‍රකට","ජනප්‍රියතම"]
+
+# ["name", "artist","genere","lyrics by","music by","key","lyrics" ]  
 # @app.route('/success/<name>')
 # def success(name):
 #    return 'welcome %s' % name
@@ -24,28 +31,61 @@ key_list =["major","minor"]
 # print(word_splitter.split("මම ගැයූ ගී"))
 @app.route('/',methods = ['GET'])
 def main():
-      return render_template('ui.html')
+      return render_template('ui.html')            
+
 
 @app.route('/',methods = ['POST'])
 def search():
-      query_request= request.form["query"].strip()
-      affix=word_splitter.split(query_request)["affix"]
-      result = es.search(index="lyrics", doc_type="doc",body={  "query": {"match" : { "genere": affix}}})
-      hits = result["hits"]["hits"]
+      boosting_list=[]
+      is_rating_query=False 
+      query_request= request.form["query"].strip().lower()
       
-      boosted_query_for_artist= es.search(index="lyrics", doc_type="doc",body={
+      token_list=tokenizer.tokenize(query_request)
+      
+      
+      # boosting_list[1] = ["artist^3" if ("ගේ" in artist) else "artist" for artist in  token_list][0]
+      # boosting_list[2] = ["genere^3" if (genere in token_list) else "genere" for genere in  genere_list][0]
+
+      if(any(x in genere_list for x in token_list)):
+            boosting_list.append("genere^2")
+      if(any(x in lyrics_by_list for x in token_list)):
+            boosting_list.append("lyrics by^2")
+      if(any(x in music_by_list for x in token_list)):
+            boosting_list.append("music by^2")
+      if(any(x in key_list for x in token_list)):
+            boosting_list.append("key^2")
+      if(any(x in artist_list for x in token_list)):
+            boosting_list.append("artist^2")
+      if(any(x in popular_list for x in token_list)):
+            is_rating_query= True
+
+      # = [ if (lyrics_by in token_list) else "lyrics by"for lyrics_by in  lyrics_by_list][0]
+      # boosting_list[4] = ["music by^3" if (music_by in token_list) else "music by"for music_by in  music_by_list][0]
+      # boosting_list[5] = ["key^3" if (music_by in token_list) else "key"for music_by in  music_by_list][0]
+
+      # affix=word_splitter.split(query_request)["affix"]
+     
+      processed_query_request=" ".join([word_splitter.split(item)["base"] for item in token_list]) 
+      # result = es.search(index="lyrics", doc_type="doc",body={  "query": {"match" : { "genere": affix}}})
+
+      boosting_list=list(dict.fromkeys(boosting_list))
+      print(query_request) 
+      print(token_list)
+      print(processed_query_request)
+      print(boosting_list) 
+      boosted_query= es.search(index="lyrics", doc_type="doc",body=
              {"query" : {
                   "multi_match" : {
-                        "query" : result,
-                        "fields" : [ "artist^3","lyrics by" ,"music by","lyrics","name" ]
+                        "query" : processed_query_request,
+                        "fields" : boosting_list
                   }
-    }
-}
+      }
+      }
 
 
-      })
+      )
 
-      
+      hits = boosted_query["hits"]["hits"]
 
       if(len(hits)==0):
             return render_template('ui.html', result = "No search result exists")
