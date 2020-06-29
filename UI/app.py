588512f56720efa5757d091658e6b5a485c6a0a4
DulanjayaChathura <sacdulanjaya.16@cse.mrt.ac.lk>
2020-06-29 17:16:29 +0530

    BUGFIX: changing the filters

--- UI/app.py
+++ UI/app.py
@@ -1,27 +1,53 @@
 from flask import Flask, redirect, url_for, request,render_template
 from elasticsearch import Elasticsearch
+from sinling import SinhalaTokenizer,preprocess, word_joiner,word_splitter
+
+
+
 
 
 app = Flask(__name__)
+tokenizer = SinhalaTokenizer()
 es = Elasticsearch()
 hits=[]
 lyrcs_list=[]
+dic={
+      ""
+
+
+}
 
 # @app.route('/success/<name>')
 # def success(name):
 #    return 'welcome %s' % name
 
 # @app.route('/search',methods = ['POST', 'GET'])
-
+# print(word_splitter.split("මම ගැයූ ගී"))
 @app.route('/',methods = ['GET'])
 def main():
       return render_template('ui.html')
 
 @app.route('/',methods = ['POST'])
 def search():
-
-      result = es.search(index="lyrics", doc_type="doc",body={  "query": {"match" : { "genere": request.form['query']}}})
+      query_request= request.form["query"].strip()
+      affix=word_splitter.split(query_request)["affix"]
+      result = es.search(index="lyrics", doc_type="doc",body={  "query": {"match" : { "genere": affix}}})
       hits = result["hits"]["hits"]
+      
+      boosted_query_for_artist= es.search(index="lyrics", doc_type="doc",body={
+             {"query" : {
+                  "multi_match" : {
+                        "query" : result,
+                        "fields" : [ "artist^3","lyrics by" ,"music by","lyrics","name" ]
+                  }
+    }
+}
+
+
+      })
+
+      
+
       if(len(hits)==0):
             return render_template('ui.html', result = "No search result exists")
       lyrcs_list=[lyrics["_source"] for lyrics in hits ]
